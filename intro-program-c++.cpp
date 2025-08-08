4b601f65056f7f83d7289a6248093dcd6b4ddb51
unknown <anirbanislamemon.online@gmail.com>
2025-08-08 22:34:29 +0200

    Intro-Code

---
+++ intro-program-c++.cpp
@@ -0,0 +1,11 @@
+#include<iostream> //Header file that include library
+using namespace std; // That make code clean and sort - cin:std---->cin>>
+
+//Main Function Code Run from Main function
+int main(){
+cout<<"Hello World"; // Just Print or output an string
+
+
+return 0;
+}
+
