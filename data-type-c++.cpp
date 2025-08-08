2a4ac8e867f61a58ffc2e2861b62e5c8a9bb5dcc
unknown <anirbanislamemon.online@gmail.com>
2025-08-08 22:58:24 +0200

    Data-Type-c++

---
+++ data-type-c++.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//boolean	1 byte	Stores true or false values
+//char	1 byte	Stores a single character/letter/number, or ASCII values
+//int	2 or 4 bytes	Stores whole numbers, without decimals
+//float	4 bytes	Stores fractional numbers, containing one or more decimals. Sufficient for storing 6-7 decimal digits
+//double	8 bytes	Stores fractional numbers, containing one or more decimals. Sufficient for storing 15 decimal digits
+
+//int data type
+int number = 5;
+cout<<"Integer Data is : "<<number<<endl;
+
+//Float Data type
+float number1 = 4.5 ;
+cout<<"Float Number Is : "<<number1<<endl;
+
+
+//Double Data type
+double number2 = 4.55;
+cout<<"Double Number is : " << number2<<endl;
+
+//Character Data type
+char letter = 'a';
+cout<< "My charater is : "<<letter<<endl;
+
+// Boolean Data Type
+bool isOpen =true;
+cout<< "Boolean Value is : "<<isOpen<<endl;
+
+
+
+ return 0;
+}
