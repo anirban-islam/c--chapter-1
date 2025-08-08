c52875eed48b3121e9a7558d09833bfed0d4e288
unknown <anirbanislamemon.online@gmail.com>
2025-08-08 22:44:05 +0200

    Output in c++

---
+++ output-c++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+//Single output
+cout<<"Anirban Islam Emon";
+
+// multiple output
+
+cout<<"Anirban Islam Emon" << " CSE Student" << "Varendra University ";
+
+ //multiple Output With Line Break using "endl"
+ cout<<endl<< "Name : Anirban Islam Emon" << endl << "CSE Student" <<endl <<"Varendra University";
+
+ return 0 ;
+}
