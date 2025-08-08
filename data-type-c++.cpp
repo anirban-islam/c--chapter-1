b6efa6468a56f966c511a202ca6e071466604070
unknown <anirbanislamemon.online@gmail.com>
2025-08-08 23:12:26 +0200

    Variable-C++

--- data-type-c++.cpp
+++ data-type-c++.cpp
@@ -7,29 +7,34 @@ int main(){
 //int	2 or 4 bytes	Stores whole numbers, without decimals
 //float	4 bytes	Stores fractional numbers, containing one or more decimals. Sufficient for storing 6-7 decimal digits
 //double	8 bytes	Stores fractional numbers, containing one or more decimals. Sufficient for storing 15 decimal digits
+// string The string type is used to store a sequence of characters (text).
+
+
 
 //int data type
 int number = 5;
-cout<<"Integer Data is : "<<number<<endl;
+cout<<"Integer value is : "<<number<<endl;
 
 //Float Data type
 float number1 = 4.5 ;
-cout<<"Float Number Is : "<<number1<<endl;
+cout<<"Float Value Is : "<<number1<<endl;
 
 
 //Double Data type
 double number2 = 4.55;
-cout<<"Double Number is : " << number2<<endl;
+cout<<"Double value is : " << number2<<endl;
 
 //Character Data type
 char letter = 'a';
-cout<< "My charater is : "<<letter<<endl;
+cout<< "My charater value is : "<<letter<<endl;
 
 // Boolean Data Type
 bool isOpen =true;
 cout<< "Boolean Value is : "<<isOpen<<endl;
 
-
+// String data type
+string myName = "Anirban Islam Emon ";
+cout<<"String Value is : " <<myName;
 
  return 0;
 }
---
+++ variable-c++.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//Variables are containers for storing data values.
+//syntax to declare a variable
+
+// data-type Name-of-Variable = value
+
+int variable1 = 5;
+double variable2 = 5.5;
+float variable3 =  4.555;
+char variable4 = 'a';
+string variable5 = "Anirban";
+bool variable6 = false;
+
+cout<<"Variable-1 : " <<variable1 <<endl<< "Variable -2 : "<<variable2<<endl<<"Variable-3 : "<<variable3<<endl<<"Variable -4 : " <<variable4 <<endl << "variable-5 : "<<variable5<<endl<<"Variable -6 : "<<variable6<<endl;
+
+
+return 0;
+}
